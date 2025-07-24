9384703bc2d2a4e576fab69b63a9d3d81cc329e5
Dan Moffat <danmoffat@gmail.com>
2025-07-25 00:52:35 +0100

    Added remove button plus styling

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,14 +4,20 @@ import Button from './Button';
 const MovieList: FC<{ movies: string[]; onAdd: () => void }> = ({ movies, onAdd }) => {
   return (
     <div className='mb-4'>
-      <div className='flex w-full items-end justify-between border-b-4 pb-2'>
+      <div className='mb-2 flex w-full items-end justify-between border-b-4 pb-2'>
         <h2 className='font-rubik text-2xl'>Movie Playlist</h2>
         <Button onClick={onAdd}>+ Add Movie</Button>
       </div>
       {movies.length ? (
-        <ul>
+        <ul className='flex flex-col gap-2'>
           {movies.map((movie, index) => (
-            <li key={index}>{movie}</li>
+            <li
+              key={index}
+              className='flex items-center justify-between rounded-sm border-3 bg-green-400 p-4 shadow-[5px_5px_0_black] dark:bg-orange-400'
+            >
+              {movie}
+              <Button>Remove</Button>
+            </li>
           ))}
         </ul>
       ) : (
--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -4,14 +4,20 @@ import Button from './Button';
 const SongList: FC<{ songs: string[]; onAdd: () => void }> = ({ songs, onAdd }) => {
   return (
     <div className='mb-4'>
-      <div className='flex w-full items-end justify-between border-b-4 pb-2'>
+      <div className='mb-2 flex w-full items-end justify-between border-b-4 pb-2'>
         <h2 className='font-rubik text-2xl'>Song Playlist</h2>
         <Button onClick={onAdd}>+ Add Song</Button>
       </div>
       {songs.length ? (
-        <ul>
+        <ul className='flex flex-col gap-2'>
           {songs.map((song, index) => (
-            <li key={index}>{song}</li>
+            <li
+              key={index}
+              className='flex items-center justify-between rounded-sm border-3 bg-green-400 p-4 shadow-[5px_5px_0_black] dark:bg-orange-400'
+            >
+              {song}
+              <Button>Remove</Button>
+            </li>
           ))}
         </ul>
       ) : (
