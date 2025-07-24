5b3859a2d869443f9d64a7b98e0db5cd4eb45689
Dan Moffat <danmoffat@gmail.com>
2025-07-24 03:51:01 +0100

    Added google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div>App works</div>;
+  return (
+    <div className='p-2'>
+      <h1 className='font-rubik mb-2 text-4xl'>Media List</h1>
+      <button className='cursor-pointer border-2 px-4 py-2'>Reset lists</button>
+    </div>
+  );
 };
 
 export default App;
