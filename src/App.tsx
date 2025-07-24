a9e9749600cac05f5d2d512ed750df8649e03c4f
Dan Moffat <danmoffat@gmail.com>
2025-07-24 21:10:08 +0100

    Added movielist and songlist components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import Button from './components/Button';
+import MovieList from './components/MovieList';
+import SongList from './components/SongList';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [songs, setSongs] = useState([]);
+
   return (
     <div className='p-2'>
       <div className='flex items-end justify-between border-b-7 border-black pb-4'>
         <h1 className='font-rubik text-4xl tracking-tighter'>Media List</h1>
         <Button onClick={() => console.log('Hello there')}>Reset lists</Button>
       </div>
+      <MovieList />
+      <SongList />
     </div>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: FC<{ children: ReactNode; [x: string]: unknown }> = ({ children, .
   return (
     <button
       {...rest}
-      className='mr-[4px] cursor-pointer border-4 bg-green-300 px-4 py-2 font-bold uppercase shadow-[5px_5px_0_black] transition-colors duration-300 hover:bg-green-50 active:translate-[4px] active:shadow-[1px_1px_0_black]'
+      className='mr-[4px] cursor-pointer border-4 bg-transparent px-4 py-2 font-bold uppercase shadow-[5px_5px_0_black] transition-colors duration-300 hover:bg-white active:translate-[4px] active:shadow-[1px_1px_0_black]'
     >
       {children}
     </button>
