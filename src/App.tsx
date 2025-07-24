f0f03ebd2a166b3e0a6288f2fa4edbe4d6b0ed7a
Dan Moffat <danmoffat@gmail.com>
2025-07-24 15:25:05 +0100

    A little more styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Button from './components/Button';
 const App = () => {
   return (
     <div className='p-2'>
-      <h1 className='font-rubik mb-2 text-4xl tracking-tighter'>Media List</h1>
-      <Button onClick={() => console.log('Hello there')}>Reset lists</Button>
+      <div className='flex items-center justify-between border-b-7 border-black pb-4'>
+        <h1 className='font-rubik text-4xl tracking-tighter'>Media List</h1>
+        <Button onClick={() => console.log('Hello there')}>Reset lists</Button>
+      </div>
     </div>
   );
 };
