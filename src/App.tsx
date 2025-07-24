c2e68ba4516f19f61bdc99208f404a7cebf17440
Dan Moffat <danmoffat@gmail.com>
2025-07-24 04:42:47 +0100

    Typescript makes me want to pull my eyes out through my mouth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Button from './components/Button';
+
 const App = () => {
   return (
     <div className='p-2'>
       <h1 className='font-rubik mb-2 text-4xl tracking-tighter'>Media List</h1>
-      <button className='cursor-pointer border-4 bg-green-300 px-4 py-2 font-bold uppercase shadow-[5px_5px_0_black] transition-colors duration-300 hover:bg-green-50 active:relative active:top-[4px] active:left-[4px] active:shadow-[1px_1px_0_black]'>
-        Reset lists
-      </button>
+      <Button onClick={() => console.log('Hello there')}>Reset lists</Button>
     </div>
   );
 };
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import type { FC, ReactNode } from 'react';
+
+const Button: FC<{ children: ReactNode; [x: string]: unknown }> = ({ children, ...rest }) => {
+  return (
+    <button
+      {...rest}
+      className='cursor-pointer border-4 bg-green-300 px-4 py-2 font-bold uppercase shadow-[5px_5px_0_black] transition-colors duration-300 hover:bg-green-50 active:relative active:top-[4px] active:left-[4px] active:shadow-[1px_1px_0_black]'
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
