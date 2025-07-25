8579d685a01a6dbf6e6f18ef6d9dd5f0bc12a13f
Dan Moffat <danmoffat@gmail.com>
2025-07-25 01:06:27 +0100

    Fleshed out titles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from './components/Button';
 import MovieList from './components/MovieList';
 import SongList from './components/SongList';
+import { faker } from '@faker-js/faker';
 
 const App = () => {
   const [movies, setMovies] = useState<string[]>([]);
@@ -13,13 +14,17 @@ const App = () => {
   };
 
   const handleMovieAdd = () => {
-    setMovies([...movies, 'Test']);
+    let movieName = `${faker.word.adjective()} ${faker.word.noun()}`;
+    movieName = movieName.charAt(0).toUpperCase() + movieName.slice(1);
+    setMovies([...movies, movieName]);
   };
 
   const handleMovieRemove = () => {};
 
   const handleSongAdd = () => {
-    setSongs([...songs, 'Test']);
+    let songName = `${faker.word.adjective()} ${faker.word.noun()}`;
+    songName = songName.charAt(0).toUpperCase() + songName.slice(1);
+    setSongs([...songs, songName]);
   };
 
   const handleSongRemove = () => {};
